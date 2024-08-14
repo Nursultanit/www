7ad33b96befa9fd4ef398fbc89009d14bfa6e69e
nursultanit <nursultanzamirovic@gmail.com>
2024-08-14 16:08:17 +0600

    fsdfdsf

---
+++ mysite/store/views.py
@@ -0,0 +1,91 @@
+from django_filters import Filter
+from rest_framework import viewsets, generics, status, permissions
+from rest_framework.request import Request
+
+from .serializers import *
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import models
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        return Response({
+            'user': {
+                'email': user.email,
+                'username': user.username,
+                'token': str(token.access_token),
+            }
+        }, status=status.HTTP_201_CREATED)
+
+
+class CostomloginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(date=request.date)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception:
+            return Response({"detail": "Неверные учетные данные"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+        user = serializer.validated_date
+        refresh = RefreshToken.for_user(user)
+        response = HttpResponseRedirect(reverse('product_list'))
+        response.set_cookie('token', str(refresh.access_token))
+        return response
+
+
+class LogoutView(generics.GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.date["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+class UzerProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializers_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['description']
+
+class ProductPhotosViewSet(viewsets.ModelViewSet):
+    queryset = ProductPhotos.objects.all()
+    serializer_class = ProductPhotosSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
